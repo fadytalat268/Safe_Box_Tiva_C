aef4a43dcd8aa064997d5cf11d100ae2a9dc3e58
beshoyalber <59097861+beshoyalber@users.noreply.github.com>
2020-03-08 16:49:24 +0200

    Update main.c
    Version 4

--- main.c
+++ main.c
@@ -7,16 +7,20 @@ Functions:
 5. Timer 
 6. LCD_Send_Msg
 7. Lock_Ctrl
+8. Input_PW
+9. Timer_Init
 */
 //==========================================
 //Functions Prototypes:
-void Reset_PW (uint8_t * Current_Password, uint8_t * New_Password);
-uint8_t Check_PW (uint8_t * Entered_Password);
+void Reset_PW (uint8_t * Current_PW, uint8_t * Entered_PW);
+uint8_t Check_PW (uint8_t * Entered_PW);
 void Wrong_PW (void);
 void Reset_Button (void);
 void Timer (void); 
 void LCD_Send_Msg(uint8_t * Msg); 
 void Lock_Control(uint8_t);
+void Timer_Init (void);
+void Input_PW (uint8_t * Entered_PW);
 
 #define Lock_Pin PIN2
 #define Lock_Port PORT_D
@@ -27,31 +31,22 @@ void Lock_Control(uint8_t);
 #define freq 1000000 // can be changed
 void SystemInit () {}
 
-void SysTick_Init (void){
+uint8_t Current_Password [4] = "0000";
+uint8_t Entered_Password [4];
+int main (){
 
+}
+
+void Timer_Init (void){
 	NVIC_ST_CTRL_R = 0;  
 	NVIC_ST_RELOAD_R  = freq - 1; // 1 second count
 	NVIC_ST_CURRENT_R = 0;
 	NVIC_ST_CTRL_R = 5;
 }
 
-int main (){
-	char counter = 0;
-	char Reset_Message[100]="To reset the password press (A), to go back press (B)"; //may need modification
-  SysTick_Init();
-	while (1){
-	 if(NVIC_ST_CTRL_R & 0x00010000){
-	   NVIC_ST_CURRENT_R = 0;
-		 counter ++;
-	 }
-	 if(counter == 5){
-		   //call the LCD
-		   LCD_SendStr(Reset_Message);
-			 break;
-	 }
-	 }
+void Input_PW(uint8_t * Entered_PW){
+	
 }
-
 void Reset_PW (uint8_t * Current_PW, uint8_t * Entered_PW){
 
 }
